6d7a30c643dd31362f8ae72177e1d0c252d26fc2
Illuminate7 <e.zagvozdkin@gmail.com>
2014-07-08 15:22:04 +0300

    Add SafeMaybe.cs

--- SafeMaybe/SafeMaybe.cs
+++ SafeMaybe/SafeMaybe.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 
-namespace Extensions.Maybe
+namespace Extensions.SafeMaybe
 {
 
-    public static class Maybe
+    public static class SafeMaybe
     {
         /// <summary>
-        /// Возвращает результат выполнения операции.
+        /// Возвращает результат выполнения операции, в случае ошибки возвращает null.
         /// </summary>
         /// <typeparam name="TInput">Тип вхорного параметра.</typeparam>
         /// <typeparam name="TResult">Тип результата выполнения операции.</typeparam>
         /// <param name="o">Входной параметр.</param>
         /// <param name="evaluator">Опереация преобразования входного параметра в результат.</param>
         /// <returns></returns>
-        public static TResult With<TInput, TResult>
+        public static TResult SafeWith<TInput, TResult>
             (this TInput o, Func<TInput, TResult> evaluator)
             where TInput : class
             where TResult : class
         {
-            return o == null ? null : evaluator(o);
+            if (o == null)
+                return null;
+            try
+            {
+                return evaluator(o);
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         /// <summary>
@@ -30,11 +39,20 @@ namespace Extensions.Maybe
         /// <param name="evaluator">Опереация преобразования входного параметра в результат.</param>
         /// <param name="failureValue">Значение которое будет возвращено по умолчанию.</param>
         /// <returns></returns>
-        public static TResult Return<TInput, TResult>
+        public static TResult SafeReturn<TInput, TResult>
             (this TInput o, Func<TInput, TResult> evaluator, TResult failureValue)
             where TInput : class
         {
-            return o == null ? failureValue : evaluator(o);
+            if (o == null)
+                return failureValue;
+            try
+            {
+                return evaluator(o);
+            }
+            catch (Exception)
+            {
+                return failureValue;
+            }
         }
 
         /// <summary>
@@ -43,7 +61,7 @@ namespace Extensions.Maybe
         /// <typeparam name="TInput">Тип вхорного параметра.</typeparam>
         /// <param name="o">Входной параметр.</param>
         /// <returns></returns>
-        public static bool ReturnSuccess<TInput>(this TInput o)
+        public static bool SafeReturnSuccess<TInput>(this TInput o)
             where TInput : class
         {
             return o != null;
@@ -56,13 +74,20 @@ namespace Extensions.Maybe
         /// <param name="o">Входной параметр.</param>
         /// <param name="evaluator">Опереация преобразования входного параметра в результат.</param>
         /// <returns></returns>
-        public static TInput If<TInput>(this TInput o, Predicate<TInput> evaluator)
+        public static TInput SafeIf<TInput>(this TInput o, Predicate<TInput> evaluator)
             where TInput : class
         {
             if (o == null)
                 return null;
 
-            return evaluator(o) ? o : null;
+            try
+            {
+                return evaluator(o) ? o : null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         /// <summary>
@@ -72,11 +97,20 @@ namespace Extensions.Maybe
         /// <param name="o">Входной параметр.</param>
         /// <param name="action">Операция, которую необходим выполнить.</param>
         /// <returns></returns>
-        public static TInput Do<TInput>(this TInput o, Action<TInput> action)
+        public static TInput SafeDo<TInput>(this TInput o, Action<TInput> action)
             where TInput : class
         {
             if (o == null) return null;
-            action(o);
+
+            try
+            {
+                action(o);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
             return o;
         }
     }
